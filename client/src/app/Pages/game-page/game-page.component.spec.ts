fe6916b69eb5f28265b81a3d6b2da52b19f475a1
jamesthompson02 <jamesthompsongithub@outlook.com>
2024-09-10 16:45:21 +0100

    Have written unit tests for both GamePageComponent and AppComponent.

--- client/src/app/Pages/game-page/game-page.component.spec.ts
+++ client/src/app/Pages/game-page/game-page.component.spec.ts
@@ -1,23 +1,133 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
+import { GamePageActions } from '../../State/game/actions/game.actions';
+
+import { MockComponents } from 'ng-mocks';
+
+import { AppState } from '../../State/game/selectors/game.selectors';
+import { TestBed } from '@angular/core/testing';
+import { GameState } from '../../State/game/reducer/game.reducer';
 import { GamePageComponent } from './game-page.component';
+import { NavbarComponent } from '../../Components/navbar/navbar.component';
+import { PickAnOptionComponent } from '../../Components/pick-an-option/pick-an-option.component';
+import { ButtonComponent } from '../../Components/button/button.component';
+import { RulesModalComponent } from '../../Components/rules-modal/rules-modal.component';
+import { ResultComponent } from '../../Components/result/result.component';
+import { mockProvider } from '@ngneat/spectator/jest';
+import { UIService } from '../../Services/UI/ui.service';
+import { of } from 'rxjs';
 
-describe('GamePageComponent', () => {
-  let component: GamePageComponent;
-  let fixture: ComponentFixture<GamePageComponent>;
+describe('GamePage Component', () => {
+  let spectator: Spectator<GamePageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GamePageComponent]
-    })
-    .compileComponents();
+  let navbar: NavbarComponent | null;
 
-    fixture = TestBed.createComponent(GamePageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  let pickAnOption: PickAnOptionComponent | null;
+
+  let result: ResultComponent | null;
+
+  let buttons: ButtonComponent[] | null;
+
+  let rulesModal: RulesModalComponent | null;
+
+  let store: MockStore<AppState>;
+
+  const mockInitialGameState: GameState = {
+    playerScore: 0,
+  };
+
+  const mockAppState: AppState = {
+    game: mockInitialGameState,
+  };
+
+  const createGamePageComponent = createComponentFactory({
+    component: GamePageComponent,
+    shallow: true,
+    declarations: [
+      MockComponents(
+        NavbarComponent,
+        PickAnOptionComponent,
+        ButtonComponent,
+        RulesModalComponent,
+        ResultComponent
+      ),
+    ],
+    providers: [
+      provideMockStore({ initialState: mockAppState }),
+      mockProvider(UIService, {
+        UIComponent$: of('pickAnOption'),
+      }),
+    ],
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  beforeEach(() => {
+    spectator = createGamePageComponent();
+
+    store = TestBed.inject(MockStore<AppState>);
+    jest.spyOn(store, 'dispatch');
+
+    navbar = spectator.query(NavbarComponent);
+
+    pickAnOption = spectator.query(PickAnOptionComponent);
+
+    result = spectator.query(ResultComponent);
+
+    buttons = spectator.queryAll(ButtonComponent);
+
+    rulesModal = spectator.query(RulesModalComponent);
+  });
+
+  it('renders the GamePage', () => {
+    expect(spectator).toBeTruthy();
+  });
+
+  it('renders correct descendant components too based on uiComponent$ observable', () => {
+    expect(navbar).toBeTruthy();
+    expect(pickAnOption).toBeTruthy();
+    expect(result).toBeFalsy();
+    expect(buttons).toHaveLength(2);
+    expect(rulesModal).toBeTruthy();
+  });
+
+  it('resets the score when the reset score button is clicked', () => {
+    let resetScoreButton;
+
+    const resetScoreFn = jest.spyOn(spectator.component, 'resetScore');
+
+    if (buttons) {
+      resetScoreButton = buttons[0];
+    } else {
+      throw new Error('No button component is rendering.');
+    }
+
+    resetScoreButton?.onClick.emit({ type: 'click' });
+
+    spectator.detectChanges();
+
+    expect(resetScoreFn).toHaveBeenCalled();
+    expect(store.dispatch).toHaveBeenCalledWith(GamePageActions.resetScore());
+  });
+
+  it('shows the rules modal when the rules button is clicked', () => {
+    let rulesButton;
+
+    const showModalFn = jest.spyOn(spectator.component, 'showModal');
+
+    const dispatchModalDisplayStatusFn =
+      spectator.inject(UIService).dispatchModalDisplayStatus;
+
+    if (buttons) {
+      rulesButton = buttons[1];
+    } else {
+      throw new Error('No button component is rendering.');
+    }
+
+    rulesButton.onClick.emit({ type: 'click' });
+
+    spectator.detectChanges();
+
+    expect(showModalFn).toHaveBeenCalled();
+    expect(dispatchModalDisplayStatusFn).toHaveBeenCalledWith(true);
   });
 });
--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,29 +1,50 @@
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+
+import { MockComponents } from 'ng-mocks';
+
 import { TestBed } from '@angular/core/testing';
+
+import { AppState } from './State/game/selectors/game.selectors';
+import { GameState } from './State/game/reducer/game.reducer';
+import { GamePageComponent } from './Pages/game-page/game-page.component';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
+describe('AppComponent Component', () => {
+  let spectator: Spectator<AppComponent>;
+
+  let gamePage: GamePageComponent | null;
+
+  let store: MockStore<AppState>;
+
+  const mockInitialGameState: GameState = {
+    playerScore: 0,
+  };
+
+  const mockAppState: AppState = {
+    game: mockInitialGameState,
+  };
+
+  const createAppComponent = createComponentFactory({
+    component: AppComponent,
+    shallow: true,
+    declarations: [MockComponents(GamePageComponent)],
+    providers: [provideMockStore({ initialState: mockAppState })],
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    spectator = createAppComponent();
+
+    store = TestBed.inject(MockStore<AppState>);
+
+    gamePage = spectator.query(GamePageComponent);
   });
 
-  it(`should have the 'client' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
+  it('renders the App', () => {
+    expect(spectator).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, client');
+  it('renders the GamePageComponent too', () => {
+    expect(gamePage).toBeTruthy();
   });
 });
